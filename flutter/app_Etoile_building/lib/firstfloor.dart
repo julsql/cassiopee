0bdeb0b66d67bdc81c2a7361660472263d9bf43b
Iris-Marjollet <irismarjollet@gmail.com>
2023-05-11 16:37:54 +0200

    Modification First floor

--- flutter/app_Etoile_building/lib/firstfloor.dart
+++ flutter/app_Etoile_building/lib/firstfloor.dart
@@ -26,7 +26,6 @@ class FirstFloor extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           textSection,
-          MonLogo(),
           Transform.rotate(
             angle: 90 * 3.14159 / 180, // rotation de 90 degrés en radians
             child: Image(
@@ -37,81 +36,5 @@ class FirstFloor extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class MonLogo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: LogoPainter(),
-      child: Container(
-        height: 200.0,
-        width: 200.0,
-      ),
-    );
-  }
-}
-
-class LogoPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.blue
-      ..strokeWidth = 2.0
-      ..style = PaintingStyle.stroke;
-
-    // dessiner un cercle
-    canvas.drawCircle(
-        Offset(size.width / 2, size.height / 2), size.width / 3, paint);
-
-    // dessiner une ligne
-    paint.color = Colors.green;
-    canvas.drawLine(Offset(size.width / 2, size.height / 2),
-        Offset(size.width / 2, size.height * 0.75), paint);
-
-    // dessiner une courbe quadratique
-    paint.color = Colors.red;
-    Path path = Path();
-    path.moveTo(size.width / 2, size.height / 2);
-    path.quadraticBezierTo(
-        size.width * 0.75, size.height * 0.5, size.width / 2, size.height / 4);
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-class Plan extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: PlanPainter(),
-      child: Container(
-        height: 200.0,
-        width: 200.0,
-      ),
-    );
-  }
-}
-
-class PlanPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 2.0
-      ..style = PaintingStyle.stroke;
-
-    var rect = Rect.fromPoints(Offset(50.0, 50.0), Offset(150.0, 150.0));
-    canvas.drawRect(rect, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
